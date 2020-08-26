98f3a15e64f2808b69c6eabb064f6005d4e84b2e
Owen <fowenpatrick@gmail.com>
2020-08-26 14:04:38 +0800

    final changes

--- client/static/script.js
+++ client/static/script.js
@@ -5,6 +5,12 @@ $(document).ready(function(){
         var owner = $('#owner').val()
         var year = $('#year').val()
         var km_driven = $('#km_driven').val()
+        $('#wait').html(
+            '<button class="btn btn-danger btn-lg">'+
+                '<span class="spinner-border spinner-border-lg"></span>'+
+                'Estimating price. Please wait.'+
+            '</button>'
+        );
         if(transmission&&fuel&&owner&&year&&km_driven){
             $('#fill').text('');
             $.post('/predict',{
@@ -15,6 +21,7 @@ $(document).ready(function(){
                 km_driven: km_driven
             },function(data,status){
                 console.log(status);
+                $('#wait').html('');
                 $('#price').val("Php "+data['estimated_price']);
             })
         }else{
