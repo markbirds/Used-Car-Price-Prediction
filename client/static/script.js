77718266348edce9e55df1005904ca99e5952b57
Owen <fowenpatrick@gmail.com>
2020-08-26 09:10:18 +0800

    error 500

--- client/static/script.js
+++ client/static/script.js
@@ -5,6 +5,11 @@ $(document).ready(function(){
         var owner = $('#owner').val()
         var year = $('#year').val()
         var km_driven = $('#km_driven').val()
+        console.log(transmission);
+        console.log(fuel);
+        console.log(owner);
+        console.log(year);
+        console.log(km_driven);
         if(transmission&&fuel&&owner&&year&&km_driven){
             $('#fill').text('');
             $.post('/predict',{
@@ -14,6 +19,8 @@ $(document).ready(function(){
                 year: year,
                 km_driven: km_driven
             },function(data,status){
+                console.log(data);
+                console.log(status);
                 $('#price').val("Php "+data['estimated_price']);
             })
         }else{
